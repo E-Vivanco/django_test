1a3a65fdceee356dfd2cbec22fd2f8e5942eb298
Elsa Beltran <vivanco.elsa@gmail.com>
2024-09-26 02:46:36 -0300

    iniciando deploy in django 07

--- proyect001/settings.py
+++ proyect001/settings.py
@@ -26,8 +26,9 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+ALLOWED_HOSTS = ['*']
 
-ALLOWED_HOSTS = ['django-test-5z5o.onrender.com','127.0.0.1']
+#ALLOWED_HOSTS = ['django-test-5z5o.onrender.com','127.0.0.1']
 
 
 # Application definition
